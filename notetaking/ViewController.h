e38d4222b6093febdc370c75035f782ea3124dbd
India Dearlove <india_dearlove@yahoo.co.uk>
2015-02-07 11:07:41 +0000

    Initial Commit

---
+++ notetaking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  notetaking
+//
+//  Created by India Dearlove on 07/02/2015.
+//  Copyright (c) 2015 India Dearlove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ notetaking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  notetaking
+//
+//  Created by India Dearlove on 07/02/2015.
+//  Copyright (c) 2015 India Dearlove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
